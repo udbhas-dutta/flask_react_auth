5ee0c9202c14384da15af190d1c25221ba7b4a95
Udbhas Dutta <udbhasdutta@gmail.com>
2024-03-09 21:41:07 +0530

    added routes for user login, register and viewing logged in user details, started work on react frontend

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,21 @@
-from flask import Flask, request, abort
+from flask import Flask, request, jsonify, session
 from flask_bcrypt import Bcrypt
 from config import ApplicationConfig
+from flask_session import Session
 from models import db, User
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
 bcrypt = Bcrypt(app)
+server_session = Session(app)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
     
-@app.route("/register")
+    
+@app.route("/register", methods=["POST"])
 def register_user():
     email = request.json["email"]
     password = request.json["password"]
@@ -20,12 +23,56 @@ def register_user():
     user_exists = User.query.filter_by(email=email).first() is not None
     
     if(user_exists):
-        abort(409)
+        return jsonify({
+            "error": "User already Exists"
+        }), 409
     
-    hashed_password = bcrypt.generate_password_hash(password)    
-    new_user = User(email=email)    
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')    
+    new_user = User(email=email, password=hashed_password)    
+    db.session.add(new_user)
+    db.session.commit()
     
-    return ""
+    return jsonify({
+        "id":new_user.id,
+        "email":new_user.email
+    })
+
+@app.route("/me")
+def get_user():
+    user_id = session.get("user_id")
+    
+    print("Session Data:", session)
+    print("User ID:", user_id)
+    
+    if not user_id:
+        return jsonify({"error":"Unauthorized"}), 401
+    
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+        "id":user.id,
+        "email":user.email
+    })
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    email = request.json["email"]
+    password = request.json["password"]
+    
+    user = User.query.filter_by(email=email).first()
+    
+    if user is None:
+        return jsonify({"error":"User Not Found"}), 401
+    
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error":"Invalid Password"}), 401
+    
+    session["user_id"] = user.id
+    
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 db = SQLAlchemy()
 
 def get_uuid():
-    return uuid4.hex
+    return str(uuid4())
 
 class User(db.Model):
     __tablename__ = "users"
